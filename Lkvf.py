Fixed shape error in Lfvf.py, changed back to sigmoid from softmax
softmax weirdly resulted in the exact same losses and an accuracy of 1 in all epochs, I guess there is some other configuration required for softmax, or?

Lkvf.py
@@ -60,15 +60,36 @@ def train():
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(6))
-    model.add(Activation('softmax'))
+    model.add(Activation('sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='sgd',
                   metrics=['accuracy'])
+
+    #reshape trainY
+    temp = []
+    
+    for i in range(trainY.shape[0]):
+    
+      temp.append(np.array([trainY[i][0], trainY[i][1], trainY[i][2], trainY[i][3], 0, 0]))
+    
+    trainY = np.array(temp)
+    #reshape ends
 
     history = model.fit(trainX, trainY, epochs=100, batch_size=bs, verbose=1)
 
     pickle.dump(history.history, open('save4.p', 'wb'))
 
     score_train = model.evaluate(trainX, trainY, batch_size=bs, verbose=1)
+
+    #reshape testY
+    temp = []
+    
+    for i in range(testY.shape[0]):
+    
+      temp.append(np.array([testY[i][0], testY[i][1], testY[i][2], testY[i][3], 0, 0]))
+    
+    testY = np.array(temp)
+    #reshape ends
+
     score_test = model.evaluate(testX, testY, batch_size=bs, verbose=1)
 
     model_yaml = model.to_yaml()